bb26961ea94de0717f2dbc051365c83cc4a40e54
Devshahmeer <theshahmeerfsd@gmail.com>
2023-08-30 19:48:58 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Flashcard from './components/Flashcard';
+
+function App() {
+  
+  return (
+    <div className="App">
+      
+        <Flashcard />
+      </div>
+  );
+}
+
+export default App
