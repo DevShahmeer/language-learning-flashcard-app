9fd1bc094a9ded20cf9ae1ab324e72bbbba37505
Devshahmeer <theshahmeerfsd@gmail.com>
2023-08-30 19:51:07 +0500

    Load flashcard data using useEffect and add flip animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-
+import { useEffect, useState } from 'react'
 import './App.css'
+import flashcardsData from './data/flashcards.json';
 import Flashcard from './components/Flashcard';
 
 function App() {
-  
+  const [flashCards, setFlashCards] = useState([]);
+
+  useEffect(() => {
+    // Simulate loading flashcards from JSON data
+    setFlashCards(flashcardsData)
+  }, []);
+
   return (
     <div className="App">
-      
-        <Flashcard />
+      <h1>Language Learning Flashcards</h1>
+      <p className="description">Practice vocabulary translation between English and Spanish with these colorful flashcards.</p>
+      <div className='flashcard-container'>
+      {flashCards.map((flashcard, index)=> (
+        <Flashcard key={index} flashcard={flashcard} />
+      ))}
       </div>
+    </div>
   );
 }
 
