b2ab836256aa3f30bde535e7e7750282a69991a1
Devshahmeer <theshahmeerfsd@gmail.com>
2023-08-30 19:52:06 +0500

    Implement random colors and toggle source/target content

--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Flashcard = () => {
-  return (
-    <div>Flashcard</div>
-  )
+function Flashcard({ flashcard }) {
+    const [showTarget, setShowTarget] = useState(false);
+    const [randomColor, setRandomColor] = useState(getRandomColor());
+
+    function getRandomColor() {
+        const colors = ['#ffc947', '#42b883', '#ff6b6b', '#8a2be2', '#ff6347', '#20b2aa', '#9370db', '#1e90ff'];
+        const randomIndex = Math.floor(Math.random() * colors.length);
+        return colors[randomIndex];
+    }
+
+    const flipCard = () => {
+        setRandomColor(getRandomColor()); // Generate new color before flipping
+        setShowTarget(!showTarget);
+    };
+
+    return (
+        <div
+        className={`flashcard ${showTarget ? 'show-target' : ''}`}
+        onClick={flipCard}
+        >
+        <div className="flashcard-inner">
+            <div className="flashcard-front" style={{ backgroundColor: randomColor }}>
+            <span className="content">{flashcard.source}</span>
+            </div>
+            <div className="flashcard-back" style={{ backgroundColor: randomColor }}>
+            <span className="content">{flashcard.target}</span>
+            </div>
+        </div>
+        </div>
+    );
 }
 
-export default Flashcard
+export default Flashcard;
